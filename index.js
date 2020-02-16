b52e0fb9d150d9cdd4d92d721b2f69b8b9ce2fb5
Lili Rodriguez <lilimrod@gmail.com>
2020-02-16 16:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments) {
   var allBeatles = [];
   for (let i = 0; i < musicians.length; i++){
-    allBeatles.push(musicians[0])
+    allBeatles.push(musicians[i] + 'plays' + instruments[i])
   }
 }
