9699daee288baa914cb880472f42cdf221450d58
Lili Rodriguez <lilimrod@gmail.com>
2020-02-16 16:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function theBeatlesPlay(musicians,instruments) {
 }
 
 function johnLennonFacts(facts){
-  var oldFacts = [facts]
+  var oldFacts = [...facts]
   var newFacts = []
   var i = 0
   while (i < oldFacts.length) {
-    newFacts.push(oldFacts[i] + " !!!")
+    newFacts.push(oldFacts[i] + "!!!")
     i++;
   }
   return newFacts
